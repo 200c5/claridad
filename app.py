faa47e325bcda94ea36e597164ac6bb0d50f18e1
200c5 <clara2005perdomo@gmail.com>
2026-04-06 18:57:27 -0300

    fix try anidado

--- app.py
+++ app.py
@@ -609,15 +609,15 @@ with tab7:
                     importados = 0
 
                     for _, row in df.iterrows():
-                        try:
-                            try:
-                    from datetime import datetime
-                    fecha_raw = row[col_fecha]
-                    if hasattr(fecha_raw, 'strftime'):
-                        fecha = fecha_raw.strftime('%Y-%m-%d')
-                    else:
-                        fecha = str(fecha_raw)[:10]
-                except:
+                       try:
+                        from datetime import datetime
+                        fecha_raw = row[col_fecha]
+                        if hasattr(fecha_raw, 'strftime'):
+                            fecha = fecha_raw.strftime('%Y-%m-%d')
+                        else:
+                            fecha = str(fecha_raw)[:10]
+                    except:
+                        fecha = str(row[col_fecha])[:10]
                     fecha = str(row[col_fecha])[:10]
     fecha_raw = row[col_fecha]
     if hasattr(fecha_raw, 'strftime'):
